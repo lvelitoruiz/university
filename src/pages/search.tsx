70d35ac940f582e8295cda2f646c0fb2bf8f7401
luis Velito <luisvelito@luiss-Mac-Studio.local>
2022-09-29 13:13:13 -0500

    create search

---
+++ src/pages/search.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Layout from "../components/Layout/Layout";
+import { 
+  ClockIcon,
+  CheckIcon,
+  MagnifyingGlassCircleIcon,
+  Bars3CenterLeftIcon,
+  HomeIcon,
+  RectangleStackIcon,
+  UserCircleIcon,
+  ShoppingCartIcon,
+  ComputerDesktopIcon
+} from '@heroicons/react/24/outline'
+import logoWhite from "../images/logo-white.png";
+import logoIso from "../images/iso.png";
+import logo from "../images/logo-full.png";
+import product1 from "../images/product-1.png";
+import product2 from "../images/product-2.png";
+import product3 from "../images/product-3.png";
+
+const Search = () => {
+  return (
+    <Layout>
+      
+    </Layout>
+  );
+};
+
+export default Search;
