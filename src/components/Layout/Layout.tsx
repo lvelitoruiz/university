3bf67ff9a0a9e0f2782426ec976ae0295846db00
Hans Caballero <hansgianfranco@gmail.com>
2022-10-26 13:09:12 -0500

    update integration & toast

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,9 +1,14 @@
 import React from "react"
+import { Toaster } from 'react-hot-toast'
 
 const Layout = ({ children }: any) => {
   return (
     <div className="parent-wrapper">
       {children}
+      <Toaster
+          position="bottom-right"
+          reverseOrder={false}
+      />
     </div>
   )
 }
